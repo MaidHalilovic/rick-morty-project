b41002caa3f89b3849eb9200de478f6eaa68c90c
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-02-17 22:18:28 +0100

    LocationsPage are done

--- src/pages/locations/Locations.js
+++ src/pages/locations/Locations.js
@@ -1,8 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./locations.css";
+import axios from "axios";
+import img from "../../images/rick-and-morty 1.png";
+import { Pagination } from "@mui/material";
 
-function locations() {
-  return <div>locations</div>;
+function Locations() {
+  const [location, setLocation] = useState([]);
+  const [page, setPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalCards, setTotalCards] = useState(126);
+  const [cardsPerPage, setCardsPerPage] = useState(20);
+
+  const fetchLocations = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://rickandmortyapi.com/api/location?page=${page}`
+      );
+      console.log(data.results);
+
+      setLocation(data.results);
+    } catch (error) {
+      console.error("Error while fething locations", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchLocations();
+  }, [currentPage]);
+
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+    setPage(value);
+  };
+
+  return (
+    <div className='main-Container'>
+      <div className='img'>
+        <img src={img} alt='img' />
+      </div>
+      <div className='card'>
+        {location.map((el, index) => (
+          <div className='cards' key={index}>
+            <h1>{el.name}</h1>
+            <h3 style={{ color: "gray" }}>{el.type}</h3>
+          </div>
+        ))}
+      </div>
+      <Pagination
+        count={Math.ceil(totalCards / cardsPerPage)}
+        page={currentPage}
+        onChange={handlePageChange}
+        style={{
+          marginBottom: 100,
+          position: "relative",
+          top: 30,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      />
+    </div>
+  );
 }
 
-export default locations;
+export default Locations;
