9f79d8d9627879edd75529411e58284b1bbd2231
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-02-19 22:59:19 +0100

    ContactUs Page are done

--- src/pages/contactUs/ContactUs.js
+++ src/pages/contactUs/ContactUs.js
@@ -1,7 +1,29 @@
 import React from "react";
+import "./contactUs.css";
+import { FaPhoneVolume } from "react-icons/fa6";
+import { CgMail } from "react-icons/cg";
+import { IoLocationSharp } from "react-icons/io5";
 
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  return (
+    <div className='main-container'>
+      <div className='contactUs'>
+        <h1>Contact Us</h1>
+        <p>Any question or remarks? Just write us a message!</p>
+      </div>
+      <div className='info'>
+        <h1>Contact Information</h1>
+        <p style={{ color: "lightgray", position: "relative", bottom: 20 }}>
+          Say something to start a live chat!
+        </p>
+        <div className='info-2'>
+          <FaPhoneVolume /> <p>0637822540</p>
+          <CgMail /> <p>maidhaliloviccc@gmail.com</p>
+          <IoLocationSharp /> <p>Novi Pazar</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContactUs;
